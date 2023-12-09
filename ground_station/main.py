a207d5b1c22b326ea108a396c4f7757352e8324c
Jose Troche <jose.troche.coder@gmail.com>
2023-12-09 15:18:14 -0500

    Updated main to use sigint handler

--- ground_station/main.py
+++ ground_station/main.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python3
 
 import multiprocessing
+import sigint_handler
+import time
 from video_receiver import video_receiver
 from webserver import webserver
-from database_keys import FRAME
+from database_keys import SHUTDOWN
 
 if __name__ == '__main__':
-    # event_bus = EventBus()
-    # event_bus.add_listener(EventBus.VIDEO_FRAME, on_video_frame_received)
 
     with multiprocessing.Manager() as manager:
         db = manager.dict()
-        db[FRAME] = b''
-
-        shutdown = multiprocessing.Event()
 
         processes = [
-            multiprocessing.Process(target=target, args=(db, shutdown))
+            multiprocessing.Process(target=target, args=(db, ))
             for target in [webserver, video_receiver]
         ]
 
         for p in processes:
             p.start()
 
-        # Ctrl+C to stop main
-        try:
-            while True:
-                pass
-        finally:
-            print("Shutting down ALL processes")
-            shutdown.set()
-            for p in processes:
-                p.join()
+        while not sigint_handler.shutdown_signal:
+            time.sleep(1)
+
+        print('Main process shutting down ...')
+
+        # Signal all processes to shutdown
+        db[SHUTDOWN] = True
+
+        # Wait for them to finish
+        for p in processes:
+            p.join()
+
+        print('Shutdown complete')
