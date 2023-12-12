90ca4ee7e8da36e7b07345b8fe777995cd5fbae5
Jose Troche <jose.troche.coder@gmail.com>
2023-12-12 06:54:05 -0500

    Added gamepad input to send drone commands

--- ground_station/webassets/drone_pilot.js
+++ ground_station/webassets/drone_pilot.js
@@ -1,3 +1,4 @@
+// -------------------------------  Video Frames update ------------------------------------
 function refreshImage() {
     const img = document.getElementById('video_frame');
     img.src = '/video_frame'
@@ -5,17 +6,7 @@ function refreshImage() {
   
 setInterval(refreshImage, 33);
 
-
-/**
- * Sends a drone command to the webserver
- */
-function sendDroneCommand(command) {
-    // console.log("Sending drone command: ", command);
-    fetch(`/drone?command=${command}`);
-}
-
-
-// --------------------------------  Recognized Objects ------------------------------------
+// -------------------------------  Recognized Objects update ------------------------------------
 async function refreshRecognizedObjects() {
     const recognizedObjects = await (await fetch('/recognized_objects')).json();
     if (recognizedObjects.length > 0) {
@@ -25,7 +16,7 @@ async function refreshRecognizedObjects() {
 }
 setInterval(refreshRecognizedObjects, 750);
 
-// --------------------------------  Refresh Fire Laser ------------------------------------
+// --------------------------------  Fire Laser flag update ------------------------------------
 async function refreshFireLaser() {
     const fireLaser = await (await fetch('/fire_laser')).json();
     const fire = document.getElementById('fire');
@@ -41,3 +32,33 @@ function setSearchObjects(){
         fetch(`/set_search_objects?search_objects=${searched_objects}`);
     }
 };
+
+// ------------------------  Send Drone commands mapped from GamePad ------------------------------
+//   Drone commands conform to:
+//   https://dl-cdn.ryzerobotics.com/downloads/tello/20180910/Tello%20SDK%20Documentation%20EN_1.3.pdf
+function sendDroneCommandFromGamepadState() {
+  const controller = navigator.getGamepads ? navigator.getGamepads()[0] : false;
+
+  if (controller) {
+    let command = "";
+
+    if (controller.buttons[3].pressed) command = "takeoff"; // X
+    else if (controller.buttons[0].pressed) command = "land"; // B
+    else if (controller.buttons[1].pressed) command = "streamon"; // A
+    else if (controller.buttons[2].pressed) command = "streamoff"; // Y
+    else if (controller.buttons[6].pressed) command = "flip l"; // ZL
+    else if (controller.buttons[7].pressed) command = "flip r"; // ZR
+    else { // Just send an rc command wth the axes data
+      const MAX = 60, DECIMALS = 4;
+      const a = (controller.axes[0]*MAX).toFixed(DECIMALS);
+      const b = (-controller.axes[1]*MAX).toFixed(DECIMALS);
+      const d = (controller.axes[2]*MAX).toFixed(DECIMALS);
+      const c = (-controller.axes[3]*MAX).toFixed(DECIMALS);
+      command = `rc ${a} ${b} ${c} ${d}`;
+    }
+
+    // Send drone command to webserver
+    fetch(`/drone?command=${command}`);
+  }
+}
+setInterval(sendDroneCommandFromGamepadState, 200);
